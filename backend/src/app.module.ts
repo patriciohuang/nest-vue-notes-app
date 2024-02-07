657a0597cb37611091e293aeb046bcf0e2615f87
patriciohuang <patriciohuang20@gmail.com>
2024-02-07 20:56:45 +0100

    Use NestJS ServeStaticModule for serving SPA static files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { NotesModule } from './notes/notes.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [NotesModule],
+  imports: [
+    NotesModule,
+    // Use NestJS ServeStaticModule for serving SPA static files
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    })
+  ]
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as express from 'express';
-import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // TODO needs to check
-  app.use(express.static(path.join(__dirname, '..', 'public')));
   await app.listen(3000);
 }
 bootstrap();
